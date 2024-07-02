dabb0711ebf69b3a59ccb2bccff7e0b7924e6230
Kazuki Tokunou <tokubd127@gmail.com>
2024-07-03 01:36:04 +0900

    旅行計画フォームコンポーネント作成（7/2）

---
+++ travel-itinerary-app/src/components/ItineraryForm.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import api from '../api';
+import { useHistory, useParams } from 'react-router-dom';
+
+const ItineraryForm = () => {
+    const [title, setTitle] = useState('');
+
+
+  return (
+    <div>ItineraryForm</div>
+  )
+}
+
+export default ItineraryForm
