91c822a6b3843fc9fb9e5ec5ef703bc141292223
kazair <tokubd127@gmail.com>
2024-07-16 01:17:36 +0900

    Macで動作しなかったためポートを変更

--- travel-itinerary-app/src/api.js
+++ travel-itinerary-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/itineraries', 
+    baseURL: 'http://localhost:5001/api/itineraries', 
 });
 
 export default api;
--- travel-itinerary-server/server.js
+++ travel-itinerary-server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/api/itineraries', itineraryRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
