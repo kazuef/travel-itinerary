6c88a372b00b11eaab9da1754383b696c6b69b13
kazair <tokubd127@gmail.com>
2024-07-23 23:52:10 +0900

    ホームのリスト表示をcard形式に変更

--- travel-itinerary-app/src/components/ItineraryList.js
+++ travel-itinerary-app/src/components/ItineraryList.js
@@ -14,23 +14,34 @@ const ItineraryList = () => {
         });
     }, []);
 
-    const handleClick =() => {
+    const handleClick = () => {
         navigate('/create');
     };
-    
+
     return (
         <div className="container">
-            <div class="d-grid gap-2 col-6 mx-auto"> 
-            <button className='btn btn-primary' onClick={handleClick}>新規しおり作成</button>
+            <div class="d-grid gap-2 col-6 mx-auto mb-3">
+                <button className='btn btn-primary' onClick={handleClick}>新規しおり作成</button>
             </div>
-            
-            <ul>
+
+            <div class="row row-cols-1 row-cols-md-4 g-4">
                 {itineraries.map((itinerary) => (
-                    <li key={itinerary.id}>
-                        <Link to={`/itinerary/${itinerary.id}`}>{itinerary.title}</Link>
-                    </li>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{itinerary.title}</h5>
+                                <p class="card-text">{itinerary.description}</p>
+                                <p>
+                                    {new Date(itinerary.startDate).toLocaleDateString()} -{' '}
+                                    {new Date(itinerary.endDate).toLocaleDateString()}
+                                </p>
+                                <Link to={`/itinerary/${itinerary.id}`} class="btn btn-primary">詳細</Link>
+                            </div>
+                            {/* <img src="..." class="card-img-top" alt="..."></img> */}
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 };
